ba7e03d9579fa806b6dba8116d108166f41d2c6d
Roronoawjd <2jw2534@gmail.com>
2023-12-09 22:43:03 +0900

    change options

--- modes/dalfox.py
+++ modes/dalfox.py
@@ -1,19 +1,10 @@
 import subprocess
 
-def dalfox(param_list, delay):
-    if delay:
-        print("[+] dalfox start")
-        for url in param_list:
-            basic_dalfox_command = f'/bin/dalfox url "{url}" --delay {delay} --waf-evasion --skip-mining-all --silence'
-            print(f"[+] query: {basic_dalfox_command}")
-            dalfox_result = subprocess.run(basic_dalfox_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            print("[+] dalfox_result:")
-            print(dalfox_result.stdout)
-    else:
-        print("[+] dalfox start")
-        for url in param_list:
-            basic_dalfox_command = f'/bin/dalfox url "{url}" --waf-evasion --skip-mining-all --silence'
-            print(f"[+] query: {basic_dalfox_command}")
-            dalfox_result = subprocess.run(basic_dalfox_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            print("[+] dalfox_result:")
-            print(dalfox_result.stdout)
+def dalfox(param_list, delay, header):
+    print("[+] dalfox start")
+    for url in param_list:
+        basic_dalfox_command = f'/bin/dalfox url "{url}" --delay {delay} -H {header} --waf-evasion --skip-mining-all --silence'
+        print(f"[+] query: {basic_dalfox_command}")
+        dalfox_result = subprocess.run(basic_dalfox_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print("[+] dalfox_result:")
+        print(dalfox_result.stdout)
