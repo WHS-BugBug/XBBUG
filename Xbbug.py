514448e509aded47216a77eae280d0b652fabaed
Roronoawjd <2jw2534@gmail.com>
2023-12-09 16:36:45 +0900

    add depth

--- Xbbug.py
+++ Xbbug.py
@@ -13,16 +13,20 @@ logo()
 parser = argparse.ArgumentParser(description='XSS automated detection framework')
 parser.add_argument('-u', '--url', required=True, help='input url ex) naver.com, enki.co.kr', dest='url')
 parser.add_argument('-ds', '-do-not-search-subdomains', action='store_true', help="Don't search for subdomains")
+parser.add_argument('-d', '-depth', help="maximum depth to crawl of katana (default 3)", default=3, dest='depth')
+
+
 
 args = parser.parse_args()
 
 url = args.url
 dont_search_subdomains = args.ds
+depth = args.depth
 
 if not dont_search_subdomains:
     subdomain_list = subdomain(url)
-    param_list = katana(subdomain_list, dont_search_subdomains)
+    param_list = katana(subdomain_list, dont_search_subdomains, depth)
     dalfox(param_list)
 else:
-    param_list = katana(url, dont_search_subdomains)
+    param_list = katana(url, dont_search_subdomains, depth)
     dalfox(param_list)
