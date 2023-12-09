8ff345323c609c6d92782618d6ff49b899cac581
Roronoawjd <2jw2534@gmail.com>
2023-12-10 02:42:29 +0900

    add worker options

--- modes/dalfox.py
+++ modes/dalfox.py
@@ -1,9 +1,9 @@
 import subprocess
 
-def dalfox(param_list, delay, header):
+def dalfox(param_list, delay, header, worker):
     print("[+] dalfox start")
     for url in param_list:
-        basic_dalfox_command = f'/bin/dalfox url "{url}" --delay {delay} -H {header} --waf-evasion --skip-mining-all --silence'
+        basic_dalfox_command = f'/bin/dalfox url "{url}" -w {worker} --delay {delay} -H {header} --waf-evasion --skip-mining-all --silence'
         print(f"[+] query: {basic_dalfox_command}")
         dalfox_result = subprocess.run(basic_dalfox_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         print("[+] dalfox_result:")
