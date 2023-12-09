3458f06dbefe95cd615ad104a7535f42b0925fdc
Roronoawjd <2jw2534@gmail.com>
2023-12-09 17:22:19 +0900

    modify delay options

--- modes/dalfox.py
+++ modes/dalfox.py
@@ -4,7 +4,7 @@ def dalfox(param_list, delay):
     if delay:
         print("[+] dalfox start")
         for url in param_list:
-            basic_dalfox_command = f'/bin/dalfox url "{url}" -d {delay}--waf-evasion --skip-mining-all --silence'
+            basic_dalfox_command = f'/bin/dalfox url "{url}" --delay {delay}--waf-evasion --skip-mining-all --silence'
             print(f"[+] query: {basic_dalfox_command}")
             dalfox_result = subprocess.run(basic_dalfox_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             print("[+] dalfox_result:")
