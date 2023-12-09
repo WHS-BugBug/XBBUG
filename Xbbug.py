8a1679867c7ab6cc08487ce3f52693e975431697
Roronoawjd <2jw2534@gmail.com>
2023-12-09 22:45:27 +0900

    modify header option

--- Xbbug.py
+++ Xbbug.py
@@ -20,7 +20,7 @@ parser.add_argument('-u', '--url', required=True, help='input url ex) naver.com,
 parser.add_argument('-ds', '-do-not-search-subdomains', action='store_true', help="Don't search for subdomains")
 parser.add_argument('-d', '-depth', help="maximum depth to crawl of katana (default 3)", default=3, dest='depth')
 parser.add_argument('-rd', '-delay', help="Milliseconds between send to same host of dalfox (1000==1s)", dest='delay', default=0)
-parser.add_argument('-H', '-header', help="Add custom headers of dalfox", dest='header', default=f"User-Agent: {user_agent}")
+parser.add_argument('-H', '-header', help="Add custom headers of dalfox", dest='header', default=f'"User-Agent: {user_agent}"')
 
 args = parser.parse_args()
 
