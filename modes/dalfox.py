f0a2185f0eb6c181f828b13c3a2cb676f8851717
Roronoawjd <2jw2534@gmail.com>
2023-12-09 15:06:51 +0900

    first commit

---
+++ modes/dalfox.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def dalfox(param_list):
+    print("[+] dalfox start")
+    for url in param_list:
+        basic_dalfox_command = f'/bin/dalfox url "{url}" --waf-evasion --skip-mining-all --silence'
+        print(f"[+] query: {basic_dalfox_command}")
+        dalfox_result = subprocess.run(basic_dalfox_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print("[+] dalfox_result:")
+        print(dalfox_result.stdout)
