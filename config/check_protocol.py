605c6cecfc422b570032372b99b172ba8de57618
Roronoawjd <2jw2534@gmail.com>
2023-12-10 04:46:43 +0900

    modify codes

--- config/check_protocol.py
+++ config/check_protocol.py
@@ -9,3 +9,15 @@ def check_protocol(target):
             target = 'http://' + target
             
     return target
+
+def check_protocol_list(target_list):
+    subdomain_list = []
+
+    for url in target_list:
+        if not url.startswith('http'):
+            try:
+                requester('https://' + url)
+                subdomain_list += ['https://' + url]
+            except:
+                subdomain_list += ['http://' + url]
+    return subdomain_list
